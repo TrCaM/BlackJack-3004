8101a3456330fcf106b72c4ab0a6ffeb4642425b
TrCaM <cmthscb@gmail.com>
2018-09-13 10:40:09 -0400

    Implement ConsoleCommandEngine (writing test)

---
+++ src/main/java/a1/blackjack/views/Console.java
@@ -0,0 +1,7 @@
+package a1.blackjack.views;
+
+import a1.blackjack.commands.Command;
+
+public interface Console {
+  public Command promptCommand();
+}
---
+++ src/test/java/a1/blackjack/commands/ConsoleCommandEngineTest.java
@@ -0,0 +1,109 @@
+package a1.blackjack.commands;
+
+import a1.blackjack.cards.Card;
+import a1.blackjack.cards.Hand;
+import a1.blackjack.cards.Suit;
+import a1.blackjack.game.Game;
+import a1.blackjack.players.Player;
+import a1.blackjack.views.Console;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
+
+public class ConsoleCommandEngineTest {
+  private static final Card S8 = new Card(Suit.SPADE, 8);
+  private static final Card H5 = new Card(Suit.HEART, 5);
+  private static final Card S5 = new Card(Suit.SPADE, 5);
+  private static final Card D7 = new Card(Suit.SPADE, 7);
+  private static final Card H6 = new Card(Suit.HEART, 6);
+  private static final Card DA = new Card(Suit.DIAMOND, 1);
+  private static final Card SK = new Card(Suit.SPADE, 13);
+  private static final Card CK = new Card(Suit.CLUB, 13);
+  private static final Card HQ = new Card(Suit.HEART, 12);
+
+  @Mock
+  private Player player;
+
+  @Mock
+  private Game game;
+
+  @Mock
+  private Console console;
+
+  @Rule
+  public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+  private CommandEngine commandEngine;
+
+  @Before
+  public void setUp() {
+  }
+
+  @Test
+  public void getNextCommand_usingFile_shouldSucceed() {
+    Hand hand = new Hand(S8, H5);
+    when(game.getActivePlayer()).thenReturn(player).thenReturn(player);
+    when(player.getPlayingHand()).thenReturn(hand).thenReturn(hand);
+    Queue<Command> commandQueue = new LinkedList<>();
+    commandQueue.add(Command.HIT);
+    commandQueue.add(Command.STAND);
+    commandEngine = new ConsoleCommandEngine(console, commandQueue);
+
+    Command chosenCommand1 = commandEngine.getNextCommand(game);
+    Command chosenCommand2 = commandEngine.getNextCommand(game);
+
+    assertThat(chosenCommand1, is(Command.HIT));
+    assertThat(chosenCommand2, is(Command.STAND));
+    verify(game, times(2)).getActivePlayer();
+    verify(player, times(2)).getPlayingHand();
+    verifyZeroInteractions(console);
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void getNextCommand_usingFile_queueError() {
+    Hand hand = new Hand(S8, H5);
+    when(game.getActivePlayer()).thenReturn(player);
+    when(player.getPlayingHand()).thenReturn(hand);
+    Queue<Command> commandQueue = new LinkedList<>();
+    commandEngine = new ConsoleCommandEngine(console, commandQueue);
+
+    commandEngine.getNextCommand(game);
+  }
+
+  @Test
+  public void getNextCommand_consoleInput_shouldSucceed() {
+    Hand hand = new Hand(S5, H5);
+    when(game.getActivePlayer()).thenReturn(player);
+    when(player.getPlayingHand()).thenReturn(hand);
+    when(console.promptCommand()).thenReturn(Command.SPLIT);
+    commandEngine = new ConsoleCommandEngine(console);
+
+    Command chosenCommand1 = commandEngine.getNextCommand(game);
+
+    assertThat(chosenCommand1, is(Command.SPLIT));
+    verify(game).getActivePlayer();
+    verify(player).getPlayingHand();
+    verify(console).promptCommand();
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void getNextCommand_chosenCommandNotAvailable_shouldThrow() {
+    Hand hand = new Hand(S8, H5);
+    when(game.getActivePlayer()).thenReturn(player);
+    when(player.getPlayingHand()).thenReturn(hand);
+    when(console.promptCommand()).thenReturn(Command.SPLIT);
+    commandEngine = new ConsoleCommandEngine(console);
+
+    commandEngine.getNextCommand(game);
+  }
+}
