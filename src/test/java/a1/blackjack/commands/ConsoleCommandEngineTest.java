cc6a6bab076deac13cab66339f691b399a227c4a
TrCaM <cmthscb@gmail.com>
2018-09-13 10:40:57 -0400

    Fix ConsoleCommandEngineTest

--- src/test/java/a1/blackjack/commands/ConsoleCommandEngineTest.java
+++ src/test/java/a1/blackjack/commands/ConsoleCommandEngineTest.java
@@ -24,12 +24,6 @@ public class ConsoleCommandEngineTest {
   private static final Card S8 = new Card(Suit.SPADE, 8);
   private static final Card H5 = new Card(Suit.HEART, 5);
   private static final Card S5 = new Card(Suit.SPADE, 5);
-  private static final Card D7 = new Card(Suit.SPADE, 7);
-  private static final Card H6 = new Card(Suit.HEART, 6);
-  private static final Card DA = new Card(Suit.DIAMOND, 1);
-  private static final Card SK = new Card(Suit.SPADE, 13);
-  private static final Card CK = new Card(Suit.CLUB, 13);
-  private static final Card HQ = new Card(Suit.HEART, 12);
 
   @Mock
   private Player player;
