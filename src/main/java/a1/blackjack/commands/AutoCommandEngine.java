ff81abd1ea32cfc0fe166fe9537770307afabf5b
TrCaM <cmthscb@gmail.com>
2018-09-12 22:22:07 -0400

    Implement getNextCommand() of AutoCommandEngine

--- src/main/java/a1/blackjack/commands/AutoCommandEngine.java
+++ src/main/java/a1/blackjack/commands/AutoCommandEngine.java
@@ -1,13 +1,17 @@
 package a1.blackjack.commands;
 
+import a1.blackjack.cards.Hand;
 import a1.blackjack.game.Game;
 
 /**
  * Automatically generate commands base on the current player's hand. It needs to access the state
  * of the game, specifically all the hands of the player who uses this engine to make the next
- * command.
+ * command:
+ * - If the hand contains two identical cards with score less than or equal 16, it will split.
+ * - If the hand has a score of 16 or less but can not split, or soft 17 it will hit
+ * - If the hand has a score of 17 or more, it will stand.
  */
-public class AutoCommandEngine implements CommandEngine{
+public class AutoCommandEngine implements CommandEngine {
   private Game game;
 
   public AutoCommandEngine(Game game) {
@@ -16,6 +20,15 @@ public class AutoCommandEngine implements CommandEngine{
 
   @Override
   public Command getNextCommand() {
-    return null;
+    Hand activeHand = game.getActivePlayer().getPlayingHand();
+    int handScore = activeHand.getHandScore();
+    if (handScore < 17 && activeHand.canSplit()) {
+      return Command.SPLIT;
+    }
+    if (handScore < 17 || activeHand.isSoft17()) {
+      return Command.HIT;
+    }
+    return Command.STAND;
   }
+
 }
