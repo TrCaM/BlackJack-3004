b24ed66f876aa8f55947d2d15bb6ddd32bf3f79b
TrCaM <cmthscb@gmail.com>
2018-09-14 13:33:50 -0400

    Fix cardInterpret test to correctly compares cards

--- src/test/java/a1/blackjack/interpreters/InterpreterTest.java
+++ src/test/java/a1/blackjack/interpreters/InterpreterTest.java
@@ -6,7 +6,7 @@ import a1.blackjack.commands.Command;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.Matchers.samePropertyValuesAs;
 import static org.junit.Assert.assertThat;
 
 public class InterpreterTest {
@@ -23,18 +23,32 @@ public class InterpreterTest {
     Interpreter.cardInterpret("H");
     Interpreter.cardInterpret("23");
   }
-
+  
   @Test
   public void cardInterpret_shouldSucceed() {
-    assertThat(Interpreter.cardInterpret("S2"), equalTo(new Card(Suit.SPADE, 1)));
-    assertThat(Interpreter.cardInterpret("D5"), equalTo(new Card(Suit.DIAMOND, 5)));
-    assertThat(Interpreter.cardInterpret("H10"), equalTo(new Card(Suit.HEART, 10)));
-    assertThat(Interpreter.cardInterpret("CJ"), equalTo(new Card(Suit.CLUB, 11)));
-    assertThat(Interpreter.cardInterpret("SQ"), equalTo(new Card(Suit.SPADE, 12)));
-    assertThat(Interpreter.cardInterpret("DK"), equalTo(new Card(Suit.DIAMOND, 13)));
-    assertThat(Interpreter.cardInterpret("DA"), equalTo(new Card(Suit.DIAMOND, 1)));
+    assertThat(
+        Interpreter.cardInterpret("S2"),
+        samePropertyValuesAs(new Card(Suit.SPADE, 2)));
+    assertThat(
+        Interpreter.cardInterpret("D5"),
+        samePropertyValuesAs(new Card(Suit.DIAMOND, 5)));
+    assertThat(
+        Interpreter.cardInterpret("H10"),
+        samePropertyValuesAs(new Card(Suit.HEART, 10)));
+    assertThat(
+        Interpreter.cardInterpret("CJ"),
+        samePropertyValuesAs(new Card(Suit.CLUB, 11)));
+    assertThat(
+        Interpreter.cardInterpret("SQ"),
+        samePropertyValuesAs(new Card(Suit.SPADE, 12)));
+    assertThat(
+        Interpreter.cardInterpret("DK"),
+        samePropertyValuesAs(new Card(Suit.DIAMOND, 13)));
+    assertThat(
+        Interpreter.cardInterpret("DA"),
+        samePropertyValuesAs(new Card(Suit.DIAMOND, 1)));
   }
-
+  
   @Test(expected = IllegalArgumentException.class)
   public void cardInterpret_shouldThrow() {
     Interpreter.cardInterpret("MM");
