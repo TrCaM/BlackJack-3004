542562e2323997c8a4bc08bb71ac039402414501
TrCaM <cmthscb@gmail.com>
2018-09-14 22:55:32 -0400

    Update the console Interface

--- src/main/java/a1/blackjack/views/Console.java
+++ src/main/java/a1/blackjack/views/Console.java
@@ -8,5 +8,18 @@ import java.util.Set;
  * Interface for the view of the game (text or graphics).
  */
 public interface Console {
+  /**
+   * From the user to input command from the available commands.
+   */
   Command promptCommand(Set<Command> availableCommands);
+
+  /**
+   * Notify through interface that the game is starting.
+   */
+  void startGame();
+
+  /**
+   * Notify user a message.
+   */
+  void notify(String message);
 }
