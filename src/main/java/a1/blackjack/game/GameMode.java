9f4e5b108d7cf404bca938aaffa8527916b909cc
TrCaM <cmthscb@gmail.com>
2018-09-15 21:18:36 -0400

    Create GameMode enum and add promptGameMode for Console interface

---
+++ src/main/java/a1/blackjack/game/GameMode.java
@@ -0,0 +1,5 @@
+package a1.blackjack.game;
+
+public enum GameMode {
+  CONSOLE, FILE
+}
--- src/main/java/a1/blackjack/views/Console.java
+++ src/main/java/a1/blackjack/views/Console.java
@@ -1,6 +1,7 @@
 package a1.blackjack.views;
 
 import a1.blackjack.commands.Command;
+import a1.blackjack.game.GameMode;
 
 import java.util.Set;
 
@@ -8,6 +9,10 @@ import java.util.Set;
  * Interface for the view of the game (text or graphics).
  */
 public interface Console {
+  /**
+   * Get the game mode for initializing the game.
+   */
+  GameMode promptGameMode();
   /**
    * From the user to input command from the available commands.
    */
